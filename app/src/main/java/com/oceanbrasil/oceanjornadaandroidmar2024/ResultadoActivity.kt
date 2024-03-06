25d347da6e68bc1f4f79fb8c77579740395819c6
Paulo Salvatore <salvatoregames@gmail.com>
2024-03-06 18:47:36 -0300

    Comportamento do botão `Voltar`

--- app/src/main/java/com/oceanbrasil/oceanjornadaandroidmar2024/ResultadoActivity.kt
+++ app/src/main/java/com/oceanbrasil/oceanjornadaandroidmar2024/ResultadoActivity.kt
@@ -2,10 +2,20 @@ package com.oceanbrasil.oceanjornadaandroidmar2024
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class ResultadoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resultado)
+
+        // Pegamos o btVoltar pelo ID
+        val btVoltar = findViewById<Button>(R.id.btVoltar)
+
+        // Criamos um listener para o evento de clique do botão
+        btVoltar.setOnClickListener {
+            // Finalizamos a tela
+            finish()
+        }
     }
 }
