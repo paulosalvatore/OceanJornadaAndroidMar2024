2b1efa7831e09908b401bae44cfe0af8130e22e5
Paulo Salvatore <salvatoregames@gmail.com>
2024-03-12 20:20:53 -0300

    Import e inicialização do Retrofit (WIP)

--- app/src/main/java/com/oceanbrasil/oceanjornadaandroidmar2024/MainActivity.kt
+++ app/src/main/java/com/oceanbrasil/oceanjornadaandroidmar2024/MainActivity.kt
@@ -2,10 +2,17 @@ package com.oceanbrasil.oceanjornadaandroidmar2024
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://ocean-api-itens.onrender.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
     }
 }
