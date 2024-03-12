c4b9eaf90879fa006ceadeb608686f7dad34e553
Paulo Salvatore <salvatoregames@gmail.com>
2024-03-12 18:39:28 -0300

    Item 3: encontrando tesouro

--- app/src/main/java/com/oceanbrasil/oceanjornadaandroidmar2024/TreasureActivity.kt
+++ app/src/main/java/com/oceanbrasil/oceanjornadaandroidmar2024/TreasureActivity.kt
@@ -1,11 +1,22 @@
 package com.oceanbrasil.oceanjornadaandroidmar2024
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class TreasureActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_treasure)
+
+        val palavraChave = intent.getStringExtra("PALAVRA_CHAVE")
+
+        val tvMensagem = findViewById<TextView>(R.id.tvMensagem)
+
+        if (palavraChave == "ouro") {
+            tvMensagem.text = "Parabéns, você encontrou o tesouro!"
+        } else {
+            tvMensagem.text = "Desculpe, continue procurando!"
+        }
     }
 }
