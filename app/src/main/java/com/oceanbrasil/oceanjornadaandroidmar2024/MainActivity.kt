42ca5be609307f6630437e671d03d87cf5b714f9
Paulo Salvatore <salvatoregames@gmail.com>
2024-03-12 20:51:47 -0300

    Configuração do acesso à API com Retrofit

--- app/src/main/java/com/oceanbrasil/oceanjornadaandroidmar2024/MainActivity.kt
+++ app/src/main/java/com/oceanbrasil/oceanjornadaandroidmar2024/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.oceanbrasil.oceanjornadaandroidmar2024
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -14,5 +18,19 @@ class MainActivity : AppCompatActivity() {
             .baseUrl("https://ocean-api-itens.onrender.com/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
+
+        val apiService = retrofit.create(ApiService::class.java)
+
+        apiService.carregarItens().enqueue(object : Callback<Array<Item>> {
+            override fun onResponse(call: Call<Array<Item>>, response: Response<Array<Item>>) {
+                response.body()?.let {
+                    Log.d("API", it.size.toString())
+                }
+            }
+
+            override fun onFailure(call: Call<Array<Item>>, t: Throwable) {
+                Log.e("API", "Erro ao carregar dados da API.", t)
+            }
+        })
     }
 }
