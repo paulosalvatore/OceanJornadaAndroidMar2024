d240c1b20305b549fa2a191e713f0940b07931d1
Paulo Salvatore <salvatoregames@gmail.com>
2024-03-12 19:05:00 -0300

    Bônus Item 2: exibindo imagem de acordo com tesouro

--- app/src/main/java/com/oceanbrasil/oceanjornadaandroidmar2024/TreasureActivity.kt
+++ app/src/main/java/com/oceanbrasil/oceanjornadaandroidmar2024/TreasureActivity.kt
@@ -2,6 +2,7 @@ package com.oceanbrasil.oceanjornadaandroidmar2024
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -13,9 +14,17 @@ class TreasureActivity : AppCompatActivity() {
         val palavraChave = intent.getStringExtra("PALAVRA_CHAVE")
 
         val tvMensagem = findViewById<TextView>(R.id.tvMensagem)
+        val ivTesouro = findViewById<ImageView>(R.id.ivTesouro)
 
         if (palavraChave == "ouro") {
-            tvMensagem.text = "Parabéns, você encontrou o tesouro!"
+            ivTesouro.setImageResource(R.drawable.ouro)
+            tvMensagem.text = "Parabéns, você encontrou o ouro!"
+        } else if (palavraChave == "prata") {
+            ivTesouro.setImageResource(R.drawable.prata)
+            tvMensagem.text = "Parabéns, você encontrou a prata!"
+        } else if (palavraChave == "bronze") {
+            ivTesouro.setImageResource(R.drawable.bronze)
+            tvMensagem.text = "Parabéns, você encontrou o bronze!"
         } else {
             tvMensagem.text = "Desculpe, continue procurando!"
         }
