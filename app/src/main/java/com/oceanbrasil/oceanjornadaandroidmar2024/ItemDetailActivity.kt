d37ac4d7218474d1ba880fce1826b815e05b4516
Paulo Salvatore <salvatoregames@gmail.com>
2024-03-19 20:26:31 -0300

    Criação da `ItemDetailActivity`

---
+++ app/src/main/java/com/oceanbrasil/oceanjornadaandroidmar2024/ItemDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.oceanbrasil.oceanjornadaandroidmar2024
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ItemDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_item_detail)
+    }
+}
