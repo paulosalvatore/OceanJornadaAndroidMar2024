2eb3b42d4288b23d7c2e1cd147b2080e0e65e5e4
Paulo Salvatore <salvatoregames@gmail.com>
2024-03-21 18:53:21 -0300

    Desafio resolvido

--- app/src/main/java/com/oceanbrasil/oceanjornadaandroidmar2024/ApiService.kt
+++ app/src/main/java/com/oceanbrasil/oceanjornadaandroidmar2024/ApiService.kt
@@ -2,6 +2,7 @@ package com.oceanbrasil.oceanjornadaandroidmar2024
 
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 data class Item(
     val id: Int,
@@ -9,7 +10,19 @@ data class Item(
     val imagem: String
 )
 
+data class ItemDetail(
+    val id: Int,
+    val nome: String,
+    val imagem: String,
+    val status: String,
+    val especie: String,
+    val genero: String,
+)
+
 interface ApiService {
     @GET("itens")
     fun carregarItens(): Call<Array<Item>>
+
+    @GET("itens/{id}")
+    fun carregarItem(@Path("id") id: Int): Call<ItemDetail>
 }
--- app/src/main/java/com/oceanbrasil/oceanjornadaandroidmar2024/ItemDetailActivity.kt
+++ app/src/main/java/com/oceanbrasil/oceanjornadaandroidmar2024/ItemDetailActivity.kt
@@ -2,7 +2,17 @@ package com.oceanbrasil.oceanjornadaandroidmar2024
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
+import com.bumptech.glide.Glide
+import com.google.android.material.chip.Chip
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class ItemDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,13 +21,41 @@ class ItemDetailActivity : AppCompatActivity() {
 
         val id = intent.getIntExtra("ID", 0)
 
-        if (id > 0) {
-            Toast.makeText(this, "Carregar API para o ID $id", Toast.LENGTH_LONG).show()
-
-            // TODO: Desafio!
-            //   Implementar a chamada para API para o endpoint /itens/:id, usando o ID recebido
-            //   Ao receber os dados, atualizar o TextView, ImageView e as 3 Chips com os valores
-            //   recebidos da API.
+        if (id == 0) {
+            return finish()
         }
+
+        Toast.makeText(this, "Carregar API para o ID $id", Toast.LENGTH_LONG).show()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://ocean-api-itens.onrender.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val apiService = retrofit.create(ApiService::class.java)
+
+        apiService.carregarItem(id).enqueue(object : Callback<ItemDetail> {
+            override fun onResponse(call: Call<ItemDetail>, response: Response<ItemDetail>) {
+                response.body()?.let {
+                    Log.d("API", it.toString())
+
+                    val tvNome = findViewById<TextView>(R.id.tvNome)
+                    val ivImagem = findViewById<ImageView>(R.id.ivImagem)
+                    val chipStatus = findViewById<Chip>(R.id.chipStatus)
+                    val chipEspecie = findViewById<Chip>(R.id.chipEspecie)
+                    val chipGenero = findViewById<Chip>(R.id.chipGenero)
+
+                    tvNome.text = it.nome
+                    Glide.with(ivImagem).load(it.imagem).into(ivImagem)
+                    chipStatus.text = "Status: ${it.status}"
+                    chipEspecie.text = "Espécie: ${it.especie}"
+                    chipGenero.text = "Gênero: ${it.genero}"
+                }
+            }
+
+            override fun onFailure(call: Call<ItemDetail>, t: Throwable) {
+                Log.e("API", "Erro ao carregar dados da API.", t)
+            }
+        })
     }
 }
